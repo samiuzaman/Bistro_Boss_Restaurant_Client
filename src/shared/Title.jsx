a95e633fccf2ad4065ce4270c4d3ce7a379fffa7
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2025-03-09 14:54:21 +0600

    Create Dynamic Title

--- src/shared/Title.jsx
+++ src/shared/Title.jsx
@@ -1,1 +1,12 @@
+import { Divider } from "keep-react";
 
+const Title = ({ title, subtitle }) => {
+  return (
+    <div className="w-72 mx-auto">
+      <p className="text-center text-[#D99904] border-b-2 py-3">{subtitle}</p>
+      <h2 className="text-center text-2xl border-b-2 py-3 font-medium">{title}</h2>
+    </div>
+  );
+};
+
+export default Title;
