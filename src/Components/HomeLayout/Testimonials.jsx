cc91bcb7f55c07cc7a2eda0b958d8eeff01838ae
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2025-03-11 14:49:52 +0600

    Create Testimonials Section

--- src/Components/HomeLayout/Testimonials.jsx
+++ src/Components/HomeLayout/Testimonials.jsx
@@ -1,15 +1,46 @@
 import Title from "../../shared/Title";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { useEffect, useState } from "react";
+import Rating from "react-rating";
+import { FaStar } from "react-icons/fa";
 
 const Testimonials = () => {
-    return (
-      <div className="my-12">
-        <Title
-          title="TESTIMONIALS"
-          subtitle="---What Our Clients Say---"
-        ></Title>
+  const [testimonials, setTestimonials] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setTestimonials(data));
+  }, []);
+  console.log(testimonials);
+  return (
+    <div className="w-11/12 lg:w-4/5 mx-auto my-16">
+      <Title title="TESTIMONIALS" subtitle="---What Our Clients Say---"></Title>
+      <div className="mt-6">
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          <div className="bg-white">
+            {testimonials?.map((testimonial) => (
+              <SwiperSlide key={testimonial._id}>
+                <div className="px-16 bg-white shadow-lg flex flex-col justify-center text-center py-8 space-y-3">
+                  <Rating
+                    initialRating={testimonial.rating}
+                    emptySymbol={<FaStar className="text-gray-400" />}
+                    placeholderSymbol={<FaStar />}
+                    fullSymbol={<FaStar className="text-yellow-500" />}
+                    className="text-2xl"
+                  />
+                  <p>{testimonial.details}</p>
+                  <p className="text-2xl text-[#CD9003]">{testimonial.name}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Testimonials;
