8c5e46015e299293964b482ff25a0bb4abe13b2d
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2025-03-16 12:42:23 +0600

    Create Contact Location Section

---
+++ src/Components/Contact/Location.jsx
@@ -0,0 +1,41 @@
+import { IoLocationSharp } from "react-icons/io5";
+import { PiPhoneCallFill } from "react-icons/pi";
+import { IoTime } from "react-icons/io5";
+
+const Location = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-6 py-12">
+      <div className="w-full">
+        <div className="bg-[#D1A054] py-2 flex justify-center text-white text-3xl">
+          <PiPhoneCallFill />
+        </div>
+        <div className="bg-[#ffffff] w-10/12 mx-auto text-center space-y-3 py-8 h-36">
+          <h4>PHONE</h4>
+          <p>+38 (012) 34 56 789</p>
+        </div>
+      </div>
+      <div className="w-full">
+        <div className="bg-[#D1A054] py-2 flex justify-center text-white text-3xl">
+          <IoLocationSharp />
+        </div>
+        <div className="bg-[#ffffff] w-10/12 mx-auto text-center space-y-3 py-8 h-36">
+          <h4>ADDRESS</h4>
+          <p>+38 (012) 34 56 789</p>
+        </div>
+      </div>
+      <div className="w-full ">
+        <div className="bg-[#D1A054] py-2 flex justify-center text-white text-3xl">
+          <IoTime></IoTime>
+        </div>
+        <div className="bg-[#ffffff] w-10/12 mx-auto text-center space-y-3 py-8 h-36">
+          <h4>WORKING HOURS</h4>
+          <p>
+            Mon - Fri: 08:00 - 22:00 <br /> Sat - Sun: 10:00 - 23:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,29 @@
-
+import Cover from "../shared/Cover";
+import Banner from "../assets/contact/banner.jpg";
+import Title from "../shared/Title";
+import { Helmet } from "react-helmet-async";
+import Location from "../Components/Contact/Location";
 
 const Contact = () => {
-    return (
-        <div>
-            Contact Section
+  return (
+    <div>
+      <Helmet>
+        <title>OurMenu | Bistro Boss</title>
+        <link rel="canonical" href="/contact" />
+      </Helmet>
+      <Cover
+        bgimg={Banner}
+        title="CONTACT US"
+        subtitle="Would you like to try a dish?"
+      ></Cover>
+      <div className="w-11/12 lg:w-5/6 xl:w-4/5 mx-auto ">
+        <div className="py-6">
+          <Title title="OUR LOCATION" subtitle="---Visit Us---"></Title>
         </div>
-    );
+        <Location></Location>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
